e1d705cdf7cbf0539115f799d66c35fcfb20e589
Mugwortcake <tjrwls4627@naver.com>
2017-05-24 16:50:05 +0900

    받아랏~!

--- app/src/main/java/com/a4kku/todaynote/MainActivity.java
+++ app/src/main/java/com/a4kku/todaynote/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("32비트는 씹 똥 컴 입니다");
+        System.out.println("맞습니다.");
         setContentView(R.layout.activity_main);
     }
 }
